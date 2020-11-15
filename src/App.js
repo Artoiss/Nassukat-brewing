e674deb501a1c102cbf6284118ab2cdcac145443
Artoiss <aarttu@windowslive.com>
2020-11-15 19:43:33 +0200

    Text fixed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Text>
       Nassukat Brewing Batch Testing Hammond #1
-      </Text>
       </header>
     </div>
   );
